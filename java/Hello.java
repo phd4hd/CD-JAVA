c9b36094d8c890274c1cde416c563e5487a19499
Dr. Peter Dillinger <104060003+phd4hsma@users.noreply.github.com>
2023-05-06 12:13:23 +0200

    Tippfehler korrigiert

--- java/Hello.java
+++ java/Hello.java
@@ -2,6 +2,6 @@
 public class Hello {
 	public static void main(String[] args) {
 		System.out.println("Vielen Dank für Ihren Einkauf!");
-		System.out.println("Können wir Ihnen vielleicht weitere Prodkte empfehlen?");
+		System.out.println("Können wir Ihnen vielleicht weitere Produkte empfehlen?");
 	}
 }
