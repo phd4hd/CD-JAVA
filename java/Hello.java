6ba1344398ee731e4dee13d7f9b40f414e3158de
Dr. Peter Dillinger <104060003+phd4hsma@users.noreply.github.com>
2023-05-07 18:05:48 +0200

    Update Hello.java

--- java/Hello.java
+++ java/Hello.java
@@ -1,7 +1,7 @@
 
-public class Hello {
+public class NachKaufabschluss {
 	public static void main(String[] args) {
 		System.out.println("Vielen Dank für Ihren Einkauf!");
-		System.out.println("Vielleicht können wir Ihnen weitere Produkte geben?");
+		System.out.println("Vielleicht können wir Ihnen weitere Produkte empfehlen?");
 	}
 }
