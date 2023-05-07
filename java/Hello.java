243b28ec01e1581ba8ca0fe89ca9d45ebf2b9919
Dr. Peter Dillinger <104060003+phd4hsma@users.noreply.github.com>
2023-05-07 13:50:43 +0200

    Text etwas überarbeitet

--- java/Hello.java
+++ java/Hello.java
@@ -2,6 +2,6 @@
 public class Hello {
 	public static void main(String[] args) {
 		System.out.println("Vielen Dank für Ihren Einkauf!");
-		System.out.println("Vielleicht können wir Ihnen weitere Empfehlungen geben?");
+		System.out.println("Vielleicht können wir Ihnen weitere Produkte geben?");
 	}
 }
